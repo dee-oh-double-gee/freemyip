253b3753a07d86d2c8441a977f7d22ebd016708a
dee-oh-double-gee <beaugraham+github@gmail.com>
2020-07-04 00:11:28 -0600

    Added freemyip update script with pushover

---
+++ freemyippushover.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#copy in your freemyip.com url with toekn and domain and MAKE SURE you add the &verbose=yes at the end
+url="https://freemyip.com/update?token=YOUR_TOKEN&domain=YOUR_DOMAIN.freemyip.com&verbose=yes"
+
+keyword="didn't change"
+
+if curl -s "$url" | grep -q "$keyword"
+then
+        echo "No update"
+else
+    domain=$(echo "$url" | grep -o -P '(?<=domain=).*(?=freemyip.com&)')
+    echo "IP updated for ${domain}freemyip.com"
+
+    APP_TOKEN="APPTOKENHERE"
+    USER_TOKEN="USERTOKENHERE"
+    MESSAGE="IP updated for ${domain}freemyip.com"
+    TITLE="IP update"
+    wget https://api.pushover.net/1/messages.json --post-data="token=$APP_TOKEN&user=$USER_TOKEN&message=$MESSAGE&title=$TITLE" -qO- > /dev/null 2>&1 &
+fi
